6c5b3b8fc1a23aebb4dce6011df4024c0d4ff5f7
ruthjomo <ruthjomo19@gmail.com>
2020-07-26 18:07:55 +0300

    added more content i files

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,15 +1,30 @@
-$(document).ready(function(){
-    $(".design").click(function(){
-        $("#design-p").toggle();
-            $("#design").toggle();
+ $(document).ready(function(){
+     $(".design").click(function(){
+         $("#design-p").toggle();
+             $("#design").toggle();
         
-    });
-    $(".development").click(function(){
-        $("#development-p").toggle();
-            $("#development").toggle();
-    });
-    $(".product").click(function(){
-        $("#product-p").toggle();
-            $("#product").toggle();
-    });                  
-});
+     });
+     $(".development").click(function(){
+         $("#development-p").toggle();
+             $("#development").toggle();
+     });
+     $(".product").click(function(){
+         $("#product-p").toggle();
+             $("#product").toggle();
+     }); 
+     $(".black-studio").mouseOver(function(){
+         $("#studio").show();
+     }).mouseOut(function(){
+         $("#studio").hide();
+     });
+    
+        
+
+    
+        
+        
+     });                               
+
+ 
+
+     
